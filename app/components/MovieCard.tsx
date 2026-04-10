16256712e6ac1c6b369a1f7a76c2c245d9e9fab4
Deon <destinyerhen@gmail.com>
2026-04-10 13:05:10 +0100

    feat: initial commit

---
+++ app/components/MovieCard.tsx
@@ -0,0 +1,8 @@
+export function MovieCard({ movie }: { movie: string }) {
+  function onfavoriteClick() {
+    console.log("something was clicked");
+  }
+
+  return;
+  <div className="c"></div>;
+}
