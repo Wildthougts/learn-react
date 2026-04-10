65b66de089a8a2564de6dcaf06dc6da76d2af86f
Deon <destinyerhen@gmail.com>
2026-04-10 18:32:40 +0100

    type error

--- app/context/MovieContext.tsx
+++ app/context/MovieContext.tsx
@@ -1,29 +1,29 @@
-import { createContext, useState, useContext, useEffect } from "react";
-import { ReactNode } from "react";
-import { json } from "stream/consumers";
-import { Movie } from "../types/movie";
+// import { createContext, useState, useContext, useEffect } from "react";
+// import { ReactNode } from "react";
+// import { json } from "stream/consumers";
+// import { Movie } from "../types/movie";
 
-const MovieContext = createContext();
+// const MovieContext = createContext();
 
-export function useMovieContext() {
-  return useContext(MovieContext);
-}
+// export function useMovieContext() {
+//   return useContext(MovieContext);
+// }
 
-export function MovieProvider({ children }: { children: ReactNode }) {
-  const [favorites, setFavorites] = useState([]);
+// export function MovieProvider({ children }: { children: ReactNode }) {
+//   const [favorites, setFavorites] = useState([]);
 
-  useEffect(() => {
-    const storedFavs = localStorage.getItem("favorites");
-    if (storedFavs) {
-      setFavorite(JSON.parse(storedFavs));
-    }
-  }, []);
+//   useEffect(() => {
+//     const storedFavs = localStorage.getItem("favorites");
+//     if (storedFavs) {
+//       setFavorite(JSON.parse(storedFavs));
+//     }
+//   }, []);
 
-  useEffect(() => {
-    localStorage.setItem("favorites", JSON.stringify(favorites));
-  }, [favorites]);
+//   useEffect(() => {
+//     localStorage.setItem("favorites", JSON.stringify(favorites));
+//   }, [favorites]);
 
-  const addToFavorites = (movie: Movie) => {};
+//   const addToFavorites = (movie: Movie) => {};
 
-  return <MovieContext.provider>{children}</MovieContext.provider>;
-}
+//   return <MovieContext.provider>{children}</MovieContext.provider>;
+// }
